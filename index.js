2a224aed89b65df3f1ceda9efb7a00e2ecc09019
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2022-05-27 10:05:24 +0200

    v3

--- index.js
+++ index.js
@@ -7,37 +7,42 @@ let fund2 = "";
 function listQ() {
     fundList = this.value;
     console.log(fundList);
-axios.get(`https://api.github.com/repos/treasuryguild/treasury-v3/contents/proposals`)
+    localStorage.setItem("fund", fundList);
+    fund = localStorage.getItem("fund");
+    axios.get(`https://api.github.com/repos/treasuryguild/treasury-v3/contents/proposals`)
     .then(response => {
     const data = response.data;
+    
     console.log(data);
     // Loop over each object in data array
     for (let i in data) {
-        localStorage.setItem("fund", fundList);
-        fund = localStorage.getItem("fund");
-        console.log(data[i].name);
+        //console.log(data[i]);
         value2[i] = data[i].name
+        var n = i;
         axios.get(`../proposals/${data[i].name}`)
         .then(response => {
             const data2 = response.data;
             fund2 = data2.fund;
             fundJ = ("Fund" + parseInt(fund2.replace( /^\D+/g, '')));
-            console.log(data2);
-            if (fundJ == fund) {    
+            console.log(i);
+            //console.log(fund);
+                
         // Get the ul with id of of userRepos
         let ul = document.getElementById('userRepos');
         // Create variable that will create li's to be added to ul
         let li = document.createElement('div');
+        if (fundJ == fund) {
         // Create the html markup for each li
         li.innerHTML = (`
-            <button onclick="goHere(${i});">${data[i].name.replace(/\..+$/, '')}</button>
-        `);
+            <button onclick="goHere(${i});">${value2[i].replace(/\..+$/, '')}</button>
+        `);}
         // Append each li to the ul
-        ul.appendChild(li);
+        ul.appendChild(li);      
         if (i == 0) {
             while (ul.hasChildNodes()) {
                 ul.removeChild(ul.firstChild);
-              }}}
+              }
+            } 
     })
     .catch(error => console.error(error)) 
     }
