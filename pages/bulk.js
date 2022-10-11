3144aa613a2187896caa514609bfc1faedd083ee
Andre-Diamond <32074058+Andre-Diamond@users.noreply.github.com>
2022-10-11 10:33:22 +0200

    new transactions

--- pages/bulk.js
+++ pages/bulk.js
@@ -178,21 +178,34 @@ window.onload = function() {
             //const {data} = await axios.get(`https://pool.pm/wallet/${walletEl}`)
             await walletStatus();
             await loadData(orgEl, repoEl, projectJ, fundJ, poolJ);
+            let bulkB = "bulk-payments"
+            let bulkADA = 0
             for (let i in bi) {
-              if (bi[i].mdVersion[0] === "1.0") {   ///This is pulling data from new version "bulk" or single "Budget items"
-               /* y = bi[i].budget.replace(/\s/g, '-')
-                for (let j in budgetI) {    
-                  if ( y == budgetI[j]) {
-                    totals[y] = totals[y] + (parseFloat(bi[i].ada));
-                    totals.outgoing = totals.outgoing + (parseFloat(bi[i].ada));
+              if (bi[i].mdVersion) {   ///This is pulling data from new version "bulk" or single "Budget items"
+                for (let k in bi[i].contributions) {
+                  console.log("Its alive", bi[i].contributions);
+                for (let j in budgetI) {  
+                  console.log("Its alive", budgetI);  
+                  if ( budgetI[j] == 'bulk-payments') {
+                    for (let m in bi[i].contributions[k].contributors) {
+                      console.log("m", totals[budgetI[j]])
+                      bulkADA = (parseFloat(bi[i].contributions[k].contributors[m].ADA?bi[i].contributions[k].contributors[m].ADA:0));
+                    //totals[budgetI[j]] = totals[budgetI[j]] + bulkADA;
+                   // totals.outgoing = totals.outgoing + bulkADA;
+                    console.log("totals", bulkADA);
+                  }
                   }        
-                }*/
+                }
+              }
+                console.log("Its alive");
               } else {
                 y = bi[i].budget.replace(/\s/g, '-')    // THis is pulling data from old metadata
                 for (let j in budgetI) {    
                   if ( y == budgetI[j]) {
-                    totals[y] = totals[y] + (parseFloat(bi[i].ada));
-                    totals.outgoing = totals.outgoing + (parseFloat(bi[i].ada));
+                    if (y !== 'bulk-payments') {
+                      totals[y] = totals[y] + (parseFloat(bi[i].ada));
+                      totals.outgoing = totals.outgoing + (parseFloat(bi[i].ada));
+                    }
                   }        
                 }
               }
